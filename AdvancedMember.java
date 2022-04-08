6e9b568049301a792f48cdafea2ba5b828cc0e9b
Josep Navarro <63362285+jnavno@users.noreply.github.com>
2022-04-08 18:26:18 -0500

    Update AdvancedMember.java
    Formatting

--- AdvancedMember.java
+++ AdvancedMember.java
@@ -5,17 +5,12 @@ public class AdvancedMember extends Subscriber {
     //Their expertise given in the task can be: programming language or computer architecture. 
     //But because there can be one or more of them, an array of Strings has been initialized to hold multiple strings as areas of expertise.
       
-    
-    
-  AdvancedMember (int mId, String mName, String phone, 
-      String sLevel, boolean paid, String[] expertAreas) {
+        
+  AdvancedMember (int mId, String mName, String phone,String sLevel, boolean paid, String[] expertAreas) {
       //we must pass on the arguments from the parent class Subscriber here
       super ( mId, "Advanced", mName, phone, sLevel, paid);//writing each member type as a string in the stack to avoid mismatching
       //Note that the member type variable is not passed on in the contructor but directly written as a string
       this.expertAreas = expertAreas;
-            
   } 
  
-
-
 }
