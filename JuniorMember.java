ddf1400b590982599ed377afc82ff8ab65fe9f49
Josep Navarro <63362285+jnavno@users.noreply.github.com>
2022-04-08 18:28:08 -0500

    Update JuniorMember.java
    Formatting

--- JuniorMember.java
+++ JuniorMember.java
@@ -3,19 +3,12 @@ public class JuniorMember extends Subscriber {
  
   private int maOfficerId;//reference to find the Officer's Id to whom they are assigned.
     
-    JuniorMember (int mId, String mName, String phone, 
-  String sLevel, boolean paid, int maOfficerId) {//because parent class Subscriber has no default constructor,
+    JuniorMember (int mId, String mName, String phone, String sLevel, boolean paid, int maOfficerId) {//because parent class Subscriber has no default constructor,
       //Passing on the arguments from the parent class Subscriber here
       super ( mId, "Junior", mName, phone, sLevel, paid);//and call them here using the super keyword
       //writing each member type as a string in the stack to avoid mismatching
       //Note that the member type variable is not passed on in the contructor but directly written as a string
       this.maOfficerId = maOfficerId;//initializing the specific attribute of JuniorMember
-    
     } 
     
-    
-    
-        
-  
-
- }
+}
