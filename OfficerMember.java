5b4b5d79684bc927376e5a1c03b5f770abcbbf6d
Josep Navarro <63362285+jnavno@users.noreply.github.com>
2022-04-08 18:30:26 -0500

    Update OfficerMember.java
    Formatting

--- OfficerMember.java
+++ OfficerMember.java
@@ -3,19 +3,13 @@ public class OfficerMember extends Subscriber {
     
   private String responsibility;//each Junior has an Officer assigned to them  
     //Their responsibilities can be: head of club, publicity officer, purchasing officer and finance officer
-    OfficerMember (int mId, String mName, String phone, 
-  String sLevel, boolean paid, String responsibility) {//because parent class Subscriber has no default constructor,
+    OfficerMember (int mId, String mName, String phone, String sLevel, boolean paid, String responsibility) {
+      //because parent class Subscriber has no default constructor,
       //we must pass on the arguments from the parent class Subscriber here
       super ( mId, "Officer", mName, phone, sLevel, paid);//and call them here using the super keyword//
       //writing each member type as a string in the stack to avoid mismatching
       //Note that the member type variable is not passed on in the contructor but directly written as a string
       this.responsibility = responsibility;//initializing the specific attribute of JuniorMember
-    
-    } 
-    
-    
-    
-        
-  
+  }
 
- }
+}
